1b3cab346ecdbb2427629e21ed8ad4660250c4e9
ohadb <ohadb@wix.com>
2016-08-06 22:48:57 +0300

    ignore the authenticity token for photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
+  skip_before_action :verify_authenticity_token
 
   # GET /photos
   # GET /photos.json
